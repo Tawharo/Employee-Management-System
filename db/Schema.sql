bd1487401f0317b2736c2fec89a1777baef41122
Tawheed Haroon <tawheed.haroon@gmail.com>
2020-06-16 07:09:05 -0400

    stuff

--- db/Schema.sql
+++ db/Schema.sql
@@ -5,24 +5,29 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE department (
-    id INT(10) AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT(10) AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL, 
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT(10) AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+SELECT * FROM ;
---
+++ db/Seeds.sql
@@ -0,0 +1,55 @@
+USE employee_trackerDB;
+
+/* departments */
+INSERT INTO department (name)
+VALUES ("Sales"), ("Accounting"), ("Management"), ("Human Resources");
+
+/* roles */
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesman", 75000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesman", 75000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesman", 75000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesman", 75000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesman", 75000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesman", 75000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesman", 75000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesman", 75000, 1);
+
+/* employees */
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tawheed", "Haroon", 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tawheed", "Haroon", 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tawheed", "Haroon", 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tawheed", "Haroon", 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tawheed", "Haroon", 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tawheed", "Haroon", 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tawheed", "Haroon", 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tawheed", "Haroon", 1);
